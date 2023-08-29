f39e1538c1e8e931952319bfa894af0f09b1c3d5
ruanzhengting <13552321519@163.com>
2023-08-29 08:40:59 +0800

    20230829new

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Index from '@/view/index/index'
 import NewOrder from '@/view/new/new'
-import NewTwo from '@/view/new(2)/index'
-// import NewThree from '@/view/new/new(3)/index'
 Vue.use(Router)
 
 export default new Router({
@@ -18,16 +16,6 @@ export default new Router({
       path: '/new',
       name: 'newOrder',
       component: NewOrder
-    },
-    {
-      path: '/two',
-      name: 'newtwo',
-      component: NewTwo
     }
-    // {
-    //   path: '/three',
-    //   name: 'newThree',
-    //   component: NewThree
-    // }
   ]
 })
